31cfdcba62037a3fb7519ba0d15960efe08c9ee4
AriellaLewis <160497915+AriellaLewis@users.noreply.github.com>
2024-04-10 16:47:15 +0000

    Add files via upload

--- api/cars.js
+++ api/cars.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const unparsedPath = path.resolve(__dirname,"cars.json");
 
-app.http('loadCars', {
+app.http('cars', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
