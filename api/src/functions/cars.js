8ac66bb8ddb130cebeb20179136908b86852dfd2
AriellaLewis <160497915+AriellaLewis@users.noreply.github.com>
2024-04-10 20:54:07 +0200

    Update cars.js

--- api/src/functions/cars.js
+++ api/src/functions/cars.js
@@ -1,19 +1,34 @@
-const { app } = require('@azure/functions');
 const fs = require('fs').promises;
 const path = require('path');
+const { app } = require('@azure/functions');
 
-const unparsedPath = path.resolve(__dirname,"cars.json");
 
+const jsonFilePath = path.resolve(__dirname,'cars.json');
+
+
+/**
+ * Returns the cars array from the cars.json file
+ */
 app.http('cars', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-    const jsonSet = await fs.readFile(unparsedPath, 'utf8');
-    const parsedData = JSON.parse(jsonSet);
-        
-        return { 
-            status:200,
-            body:  JSON.stringify(parsedData)       
-        };
-    }
+        try{
+        const jsonData = await fs.readFile(jsonFilePath, 'utf8');
+            const data = JSON.parse(jsonData);
+
+            return {
+                status:200,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            };
+        }catch(error){
+            return{
+                status:500,
+                body:'Internal Server Error. Failed to get cars.'
+            }
+        }
+    }
 });
