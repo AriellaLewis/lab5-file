0ddbeb936a6feeeaaad3a1e1449f916e33feeeb4
AriellaLewis <160497915+AriellaLewis@users.noreply.github.com>
2024-04-10 22:58:32 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -71,7 +71,7 @@ function removeCar(index) {
             loadCarsBtn.click();
         })
         .catch(error => {
-            console.error('Error:', JSON.stringify(error));
+            console.error('Error:', error);
         });
 }
 // Event delegation for remove buttons
